bdac11fa11fe9a54d27b840855cbb5d8f5d6960b
UWParlay <UWParlay>
2019-06-03 15:44:00 -0700

    added state map

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function render() {
   var y = d3.scaleLinear().range([height, 0]);
 
   var margin = { top: 40, right: 100, bottom: 50, left: 100 },
-    width = 900 - margin.left - margin.right,
-    height = 531.25 - margin.top - margin.bottom;
+    width = 900 - margin.left - margin.right ,
+    height = 531.25 - margin.top - margin.bottom 
 
   // define the line
   var valueline = d3
@@ -326,6 +326,8 @@ function render() {
 
   var path = svg2.append("path");
 
+  // USA MAP
+  
   var gs2 = d3
     .graphScroll()
     .container(d3.select(".container-2"))
@@ -333,40 +335,120 @@ function render() {
     .eventId("uniqueId2") // namespace for scroll and resize events
     .sections(d3.selectAll(".container-2 #sections > div"))
     .on("active", function(i) {
-      var h = height;
-      var w = width;
-      var dArray = [
-        [
-          [w / 4, h / 4],
-          [(w * 3) / 4, h / 4],
-          [(w * 3) / 4, (h * 3) / 4],
-          [w / 4, (h * 3) / 4]
-        ],
-        [
-          [0, 0],
-          [(w * 3) / 4, h / 4],
-          [(w * 3) / 4, (h * 3) / 4],
-          [w / 4, (h * 3) / 4]
-        ],
-        [[w / 2, h / 2], [w, h / 4], [w, h], [w / 4, h]],
-        [[w / 2, h / 2], [w, h / 4], [w, h], [w / 4, h]],
-        [[w / 2, h / 2], [w, h / 2], [0, 0], [w / 4, h / 2]],
-        [[w / 2, h / 2], [0, h / 4], [0, h / 2], [w / 4, 0]]
-      ].map(function(d) {
-        return "M" + d.join(" L ");
+      var width = 960;
+      var height = 500;
+      
+      var lowColor = "white"
+      var highColor = "red"
+      
+      // D3 Projection
+      var projection = d3.geoAlbersUsa()
+        .translate([width/3, height / 2]) // translate to center of screen
+        .scale([850]); // scale things down so see entire US
+      
+      // Define path generator
+      var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
+        .projection(projection); // tell path generator to use albersUsa projection
+      
+      // Load in my states data!
+      d3.csv("statedata.csv", function(data) {
+        var dataArray = [];
+        for (var d = 0; d < data.length; d++) {
+          dataArray.push(parseFloat(data[d].value))
+        }
+        var minVal = d3.min(dataArray)
+        var maxVal = d3.max(dataArray)
+        var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor])
+        
+        // Load GeoJSON data and merge with states data
+        d3.json("us-states.json", function(json) {
+      
+          // Loop through each state data value in the .csv file
+          for (var i = 0; i < data.length; i++) {
+            // Grab State Name
+            var dataState = data[i].state;
+            // Grab data value 
+            var dataValue = data[i].value;
+      
+            // Find the corresponding state inside the GeoJSON
+            for (var j = 0; j < json.features.length; j++) {
+              var jsonState = json.features[j].properties.name;
+      
+              if (dataState == jsonState) {
+      
+                // Copy the data value into the JSON
+                json.features[j].properties.value = dataValue;
+      
+                // Stop looking through the JSON
+                break;
+              }
+            }
+          }
+      
+          // Bind the data to the SVG and create one path per GeoJSON feature
+          svg2.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("stroke", "#fff")
+            .style("stroke-width", "1")
+            .style("fill", function(d) { return ramp(d.properties.value) });
+          
+          // add a legend
+          var w = 140, h = 300;
+      
+          var key = d3.select(".container-2")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "legend");
+      
+          var legend = key.append("defs")
+            .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "100%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+      
+          legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", highColor)
+            .attr("stop-opacity", 1);
+            
+          legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", lowColor)
+            .attr("stop-opacity", 1);
+      
+          key.append("rect")
+            .attr("width", w - 100)
+            .attr("height", h)
+            .style("fill", "url(#gradient)")
+            .attr("transform", "translate(0,10)");
+      
+          var y = d3.scaleLinear()
+            .range([h, 0])
+            .domain([minVal, maxVal]);
+      
+          var yAxis = d3.axisRight(y);
+      
+          key.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(41,10)")
+            .call(yAxis)
+        });
       });
 
-      path
-        .transition()
-        .duration(1000)
-        .attr("d", dArray[i])
-        .style("fill", colors[i]);
-    });
+    
+    })
 
-  d3.select("#source").styles({
-    "margin-bottom": window.innerHeight - 450 + "px",
-    padding: "100px"
-  });
+  // d3.select("#source").styles({
+  //   "margin-bottom": window.innerHeight - 450 + "px",
+  //   padding: "100px"
+  // });
 }
 render();
 d3.select(window).on("resize", render);
